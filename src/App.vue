fcef1095e1a7b86d053ad914125a20a87c7c61ba
乃木流架 <1693145038@qq.com>
2025-01-01 00:53:42 +0900

    fix: 修复类型检查问题

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,15 @@ import { computed } from 'vue'
 
 const notesStore = useNotesStore()
 const { notes, selectedNoteId } = storeToRefs(notesStore)
-const selectedNote = computed(() => notesStore.getSelectedNote())
+const selectedNote = computed(() => {
+  const note = notesStore.getSelectedNote()
+  return note ? {
+    ...note,
+    updateTitle: (title: string) => notesStore.updateNote(note.id, { title }),
+    updateContent: (content: string) => notesStore.updateNote(note.id, { content, html: content }),
+    delete: () => notesStore.deleteNote(note.id)
+  } : null
+})
 
 function handleNoteSelect(note: Note) {
   notesStore.selectNote(note.id)
@@ -48,18 +56,18 @@ function handleNoteDelete(id: string) {
               <input
                 v-model="selectedNote.title"
                 class="text-2xl font-bold bg-transparent border-none focus:outline-none flex-1"
-                @input="notesStore.updateNote(selectedNote.id, { title: selectedNote.title })"
+                @input="selectedNote.updateTitle(selectedNote.title)"
               >
               <button
                 class="btn bg-red-500 hover:bg-red-600"
-                @click="handleNoteDelete(selectedNote.id)"
+                @click="selectedNote.delete()"
               >
                 删除
               </button>
             </div>
             <NoteEditor
               v-model="selectedNote.content"
-              @update:modelValue="(content) => handleNoteUpdate(selectedNote.id, content)"
+              @update:modelValue="selectedNote.updateContent"
             />
           </div>
           <div v-else class="bg-white rounded-lg shadow p-4 h-[calc(100vh-4rem)] flex-center text-gray-400">
